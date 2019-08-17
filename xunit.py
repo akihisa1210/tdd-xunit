19840e59bf5374e0344e6151232e5fe134ba7dbb
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2019-08-17 23:49:42 +0900

    move TestCaseTest

--- xunit.py
+++ xunit.py
@@ -11,6 +11,15 @@ class TestCase:
         method()
 
 
+class WasRun(TestCase):
+    def set_up(self):
+        self.was_run = None
+        self.was_set_up = 1
+
+    def test_method(self):
+        self.was_run = 1
+
+
 class TestCaseTest(TestCase):
     def set_up(self):
         self.test = WasRun("test_method")
@@ -22,15 +31,6 @@ class TestCaseTest(TestCase):
     def test_set_up(self):
         self.test.run()
         assert self.test.was_set_up
-
-
-class WasRun(TestCase):
-    def set_up(self):
-        self.was_run = None
-        self.was_set_up = 1
-
-    def test_method(self):
-        self.was_run = 1
 
 
 TestCaseTest("test_running").run()
