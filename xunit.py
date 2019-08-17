b88aefff4dfb5e9287e32bfc1bd57760201a9be7
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2019-08-17 23:47:34 +0900

    call set_up first

--- xunit.py
+++ xunit.py
@@ -2,26 +2,36 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def set_up(self):
+        pass
+
     def run(self):
+        self.set_up()
         method = getattr(self, self.name)
         method()
 
 
 class TestCaseTest(TestCase):
+    def set_up(self):
+        self.test = WasRun("test_method")
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert(not test.was_run)
-        test.run()
-        assert test.was_run
+        self.test.run()
+        assert self.test.was_run
+
+    def test_set_up(self):
+        self.test.run()
+        assert self.test.was_set_up
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
+    def set_up(self):
         self.was_run = None
-        super().__init__(name)
+        self.was_set_up = 1
 
     def test_method(self):
         self.was_run = 1
 
 
 TestCaseTest("test_running").run()
+TestCaseTest("test_set_up").run()
